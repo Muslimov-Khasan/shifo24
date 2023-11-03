82795d5de30cd02c1f4453fc10f5764010b3aa08
Muslimov-Xasan <muslimovxasan050@gmail.com>
2023-11-03 11:36:06 +0500

    If you don't log in, don't go to the home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     // Retrieve the form data from localStorage
     const formData = JSON.parse(localStorage.getItem("formData"));
+
+    // If the form data doesn't exist or the user is not logged in, navigate to the login page
+    if (!formData || !formData.isLoggedIn) {
+      navigate("/");
+    }
 
     // Use the form data in the header component
     console.log(formData);
