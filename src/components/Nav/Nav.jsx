7f60483fd268321b4d3d54545afdff028ba7d351
Muslimov-Xasan <muslimovxasan050@gmail.com>
2023-11-10 15:59:10 +0500

    change style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,6 +4,9 @@ import "./Nav.scss";
 import Logo from "../../assets/img/logo.svg";
 import userIcon from "../../assets/icon/user-add.svg";
 import users from "../../assets/icon/users.svg";
+import Doctor from "../../assets/icon/doctor.svg";
+import nootbook from "../../assets/icon/nootbook.svg";
+import Setting from "../../assets/icon/setting.svg";
 
 const Nav = () => {
   return (
@@ -11,10 +14,16 @@ const Nav = () => {
       <div className="container">
         <div className="no">
           <NavLink to="/" className="logo-link">
-            <img className="logo" src={Logo} alt="logo" width={135} height={40} />
+            <img
+              className="logo"
+              src={Logo}
+              alt="logo"
+              width={135}
+              height={40}
+            />
           </NavLink>
           <ul className="nav-menu">
-              <p className="auto">avtorizatsiya</p>
+            <p className="auto">avtorizatsiya</p>
             <li className="nav-item">
               <NavLink to="/dashboard" className="nav-link">
                 <img className="icon" src={userIcon} alt="userIcon" />
@@ -23,14 +32,26 @@ const Nav = () => {
             </li>
             <li className="nav-item">
               <NavLink to="/Foydalanuvchilar" className="nav-link">
-              <img className="icon" src={users} alt="userIcon" />
+                <img className="icon" src={users} alt="userIcon" />
                 <span className="link-text">Foydalanuvchilar</span>
               </NavLink>
             </li>
           </ul>
-          <p className="app-text">
-          Mening ilovam
-          </p>
+          <p className="app-text">Mening ilovam</p>
+          <ul className="icon-list">
+            <li className="icon-item">
+              <img src={Doctor} alt="Doctor" width={25} />
+            </li>
+            <li className="icon-item">
+              <img src={userIcon} alt="user" width={25} />
+            </li>
+            <li className="icon-item">
+              <img src={nootbook} alt="Laptop" width={25} />
+            </li>
+          </ul>
+          <div className="setting">
+            <img src={Setting} alt="setting" width={25} />
+          </div>
         </div>
       </div>
     </nav>
